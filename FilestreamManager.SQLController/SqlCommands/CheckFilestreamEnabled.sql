999ab4d41dfe15cc8ea9e782f67e5f5dec7042cc
wfoster232 <wfoster232@gmail.com>
2015-03-12 21:11:20 +0800

    Initial Commit

---
+++ FilestreamManager.SQLController/SqlCommands/CheckFilestreamEnabled.sql
@@ -0,0 +1,3 @@
+﻿SELECT CAST(CASE WHEN [value] > 0 THEN 1 ELSE 0 END AS BIT) AS IsFilestreamEnabled
+FROM sys.configurations AS serverConfig
+WHERE name = 'filestream access level';
---
+++ FilestreamManager.SQLController/SqlCommands/FilestreamCompatibleTables.sql
@@ -0,0 +1,23 @@
+﻿SELECT TableName = '[' + [dbSchema].[name] + '].[' + [dbTable].[name] + ']'
+	, FilestreamAlreadyEnabled = ISNULL(FilestreamCheck.HasFilestreamColumn, 0)
+FROM [$DATABASE$].[sys].[tables] AS dbTable
+	INNER JOIN [$DATABASE$].[sys].[schemas] AS dbSchema ON [dbSchema].[schema_id] = [dbTable].[schema_id]
+	OUTER APPLY
+	(
+		-- Table does not currently have a filestream column enabled
+		SELECT TOP 1 1 AS HasFilestreamColumn
+		FROM [$DATABASE$].[sys].[columns] AS dbColumn
+			INNER JOIN [$DATABASE$].[sys].[types] AS dataType ON [dataType].[system_type_id] = [dbColumn].[system_type_id]
+		WHERE [dbTable].[object_id] = [dbColumn].[object_id]
+			AND [dataType].[name] = 'varbinary' AND [dbColumn].[max_length] = -1
+			AND [dbColumn].[is_filestream] = 1
+	) AS FilestreamCheck
+WHERE EXISTS
+	(
+		-- Table has at least one valid filestream compatible column
+		SELECT TOP 1 1 AS ValidColumn
+		FROM [$DATABASE$].[sys].[columns] AS dbColumn
+			INNER JOIN [$DATABASE$].[sys].[types] AS dataType ON [dataType].[system_type_id] = [dbColumn].[system_type_id]
+		WHERE [dbTable].[object_id] = [dbColumn].[object_id]
+			AND [dataType].[name] = 'varbinary' AND [dbColumn].[max_length] = -1
+	)
